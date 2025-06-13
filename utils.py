017333d9c515cab9a8491fcb12138645290ba44f
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-13 07:49:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
-import json
 import streamlit as st
 from fyers_apiv3 import fyersModel
 
 @st.cache_resource
 def get_fyers_client():
-    with open('config1.json') as config_file:
-        config = json.load(config_file)
-    client_id = config['client_id']
-    access_token = config['access_token']
+    client_id = st.secrets["fyers_client_id"]
+    access_token = st.secrets["fyers_access_token"]
     fyers = fyersModel.FyersModel(client_id=client_id, token=access_token)
     return fyers
