d0a6340e4fbfa5cb42eb48970fa55657575d103b
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-14 14:22:30 +0530

    Update modify_cancel_order.py

--- pages/modify_cancel_order.py
+++ pages/modify_cancel_order.py
@@ -6,12 +6,24 @@ def show():
     order_id = st.text_input("Order ID to Modify/Cancel")
     action = st.radio("Action", ["Modify", "Cancel"])
 
+    if not order_id:
+        st.info("Please enter an Order ID.")
+        return
+
     if action == "Modify":
         qty = st.number_input("New Qty", min_value=1, step=1)
         limit_price = st.number_input("New Limit Price", value=0.0)
         stop_price = st.number_input("New Stop Price", value=0.0)
-        order_type = st.selectbox("Order Type", [("Limit", 1), ("Market", 2), ("Stop (SL-M)", 3), ("Stoplimit (SL-L)", 4)], format_func=lambda x:x[0])[1]
-        side = st.selectbox("Side", [("Buy", 1), ("Sell", -1)], format_func=lambda x: x[0])[1]
+        order_type = st.selectbox(
+            "Order Type",
+            [("Limit", 1), ("Market", 2), ("Stop (SL-M)", 3), ("Stoplimit (SL-L)", 4)],
+            format_func=lambda x: x[0]
+        )[1]
+        side = st.selectbox(
+            "Side",
+            [("Buy", 1), ("Sell", -1)],
+            format_func=lambda x: x[0]
+        )[1]
         if st.button("Modify Order"):
             data = {
                 "id": order_id,
