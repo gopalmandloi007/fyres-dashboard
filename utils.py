e196ed0c0f4f30c63177af2d73ee52f8833cd498
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-14 09:49:53 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from fyers_apiv3 import fyersModel
-
-@st.cache_resource
-def get_fyers_client():
-    client_id = st.secrets["fyers_client_id"]
-    access_token = st.secrets["fyers_access_token"]
-    fyers = fyersModel.FyersModel(client_id=client_id, token=access_token)
-    return fyers
