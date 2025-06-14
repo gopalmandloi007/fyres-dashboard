a3d74d1adcabb52f0ecbf053e677fe7ef1a4a39a
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-14 10:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import importlib
 
+st.set_page_config(page_title="Fyres Dashboard", layout="wide")
+
+PAGES = {
+    "Holdings": "holdings",
+    "Positions": "positions",
+    "Orders": "orders",
+    "Trades": "trades",
+    "GTT": "gtt",
+    "Charts": "chart"
+    # Add more as needed
+}
+
+st.title("Fyres Dashboard")
+page = st.sidebar.radio("Go to", list(PAGES.keys()))
+modulename = PAGES[page]
+module = importlib.import_module(f"pages.{modulename}")
+module.show()
